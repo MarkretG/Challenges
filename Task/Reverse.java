8f1da59ff7b0e3a55752634b0962165ce7b0efe1
MARKRET G <84516916+MarkretG@users.noreply.github.com>
2021-05-21 22:19:16 +0530

    small changes

--- Task/Reverse.java
+++ Task/Reverse.java
@@ -2,17 +2,9 @@ package Task;
 
 import java.util.Scanner;
 
-public class Reverse {
-  /* public  static void rotate(String[] s,int len)
-    {
-        int i=0,j=len-1;
-        if(i==j)
-            break;
-        else
-            rotate(str[i]);
-
-    }*/
-    public  static  void main(String[] args)
+public class Reverse
+{
+   public  static  void main(String[] args)
     {
         int i,j;
         String str;
@@ -20,18 +12,7 @@ public class Reverse {
         str=sc.nextLine();
         String strarray[]=str.split(" ");
         int len=strarray.length;
-        /*for(i=strarray.length-1;i>=0;i--)
-            System.out.print(strarray[i]+" ");*/
-
-
-      /*  for(i=0,j=len-1;i<len&&j>=0;i++,j--)
-        {
-            if(i==j)
-                break;
-            else
-                rotate(strarray[i],strarray[j]);
-        }
-        for(i=0;i<len-1;i++)
-            System.out.println(strarray[i]);*/
+        for(i=strarray.length-1;i>=0;i--)
+            System.out.print(strarray[i]+" ");
     }
 }
